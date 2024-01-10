187bdad1d33f5f035f926003230edcb3e839eca8
Ruizhen Gu <ravenna1031@gmail.com>
2024-01-10 15:35:03 +0000

    add form in the forms page

--- gradeer-plus-frontend/src/views/FormsView.vue
+++ gradeer-plus-frontend/src/views/FormsView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive, ref } from 'vue'
-import { mdiBallotOutline, mdiAccount, mdiUpload, mdiPlus, mdiDelete } from '@mdi/js'
+import { mdiBallotOutline, mdiUpload, mdiPlus, mdiDelete } from '@mdi/js'
 import SectionMain from '@/components/SectionMain.vue'
 import CardBox from '@/components/CardBox.vue'
 import CardBoxComponentTitle from '@/components/CardBoxComponentTitle.vue'
@@ -20,7 +20,7 @@ import NotificationBarInCard from '@/components/NotificationBarInCard.vue'
 //   { id: 1, label: 'ManualCheck' },
 // ]
 
-const form = reactive({
+const forms = reactive([{
   type: 'ManualCheck',
   name: 'Reporting quality (to console) of answers to the questions',
   prompt: 'Reporting quality (to console) of answers to the questions:',
@@ -35,7 +35,7 @@ const form = reactive({
   arbitraryFeedback: 'False',
   priority: 38,
   checkGroup: 'FD Reporting'
-})
+}])
 
 const customElementsForm = reactive({
   checkbox: ['lorem'],
@@ -63,6 +63,25 @@ const formStatusSubmit = () => {
 const uploadFile = () => {
   console.log("upload file")
 }
+
+const addCheck = () => {
+  forms.push({
+    type: '',
+    name: '',
+    prompt: '',
+    weight: 1,
+    maxRange: 10,
+    feedbackValues: [
+      { 'score': 1.0, 'feedback': '' },
+      { 'score': 0.8, 'feedback': '' },
+      { 'score': 0.5, 'feedback': '' },
+      { 'score': 0.0, 'feedback': '' }
+    ],
+    arbitraryFeedback: 'False',
+    priority: 0,
+    checkGroup: ''
+  })
+}
 </script>
 
 <template>
@@ -72,8 +91,9 @@ const uploadFile = () => {
         <BaseButton target="_blank" :icon="mdiUpload" label="Upload Checks" color="contrast" rounded-full small
           @click="uploadFile" />
       </SectionTitleLineWithButton>
-      <CardBox form @submit.prevent="submit">
-        <CardBoxComponentTitle title="Check 1" />
+
+      <CardBox v-for="(form, index) in forms" :key="index" form @submit.prevent="submit">
+        <CardBoxComponentTitle :title="'Check ' + (index + 1)" />
 
         <div class="flex flex-row items-center mb-2">
           <label class="font-bold flex-grow-0 w-1/6">Type</label>
@@ -91,15 +111,15 @@ const uploadFile = () => {
         </div>
 
         <FormField label="Feedback Values">
-          <div v-for="(item, index) in form.feedbackValues" :key="index" class="mb-4 p-4 border border-black rounded-lg">
+          <div v-for="(item, id) in form.feedbackValues" :key="id" class="mb-4 p-4 border border-black rounded-lg">
             <div class="flex flex-row items-center mb-2">
-              <label for="score-{{ index }}" class="font-semibold flex-grow-0 w-1/6">Score</label>
-              <input id="score-{{ index }}" v-model="item.score" type="number" step="0.1" min="0" max="1"
+              <label for="score-{{ id }}" class="font-semibold flex-grow-0 w-1/6">Score</label>
+              <input id="score-{{ id }}" v-model="item.score" type="number" step="0.1" min="0" max="1"
                 class="p-2 border rounded flex-grow-0 w-1/3" />
             </div>
             <div class="flex flex-row items-center mb-2">
-              <label for="feedback-{{ index }}" class="font-semibold flex-grow-0 w-1/6">Feedback</label>
-              <input id="feedback-{{ index }}" v-model="item.feedback" rows="1"
+              <label for="feedback-{{ id }}" class="font-semibold flex-grow-0 w-1/6">Feedback</label>
+              <input id="feedback-{{ id }}" v-model="item.feedback" rows="1"
                 class="p-2 border rounded flex-grow-0 w-5/6" />
             </div>
           </div>
@@ -118,66 +138,17 @@ const uploadFile = () => {
         <div class="flex flex-row items-center mb-2">
           <label class="font-bold flex-grow-0 w-1/6">Check Group</label>
           <input v-model="form.checkGroup" class="p-2 border rounded flex-grow-0 w-1/3" />
+          <div class="ml-auto">
+            <BaseButton v-if="index == forms.length - 1" color="success" label="Add" :icon="mdiPlus" @click="addCheck" />
+            <BaseButton class="ml-2" color="danger" label="Delete" :icon="mdiDelete" />
+          </div>
         </div>
-
-        <BaseButtons>
-          <BaseButton color="success" label="Add" :icon="mdiPlus" />
-          <BaseButton color="danger" label="Delete" :icon="mdiDelete" />
-        </BaseButtons>
-
-        <template #footer>
-          <BaseButtons>
-            <BaseButton type="submit" color="info" label="Submit" />
-            <BaseButton type="reset" color="info" outline label="Reset" />
-          </BaseButtons>
-        </template>
-      </CardBox>
-    </SectionMain>
-
-    <SectionTitle>Custom elements</SectionTitle>
-
-    <SectionMain>
-      <CardBox>
-        <FormField label="Checkbox">
-          <FormCheckRadioGroup v-model="customElementsForm.checkbox" name="sample-checkbox"
-            :options="{ lorem: 'Lorem', ipsum: 'Ipsum', dolore: 'Dolore' }" />
-        </FormField>
-
-        <BaseDivider />
-
-        <FormField label="Radio">
-          <FormCheckRadioGroup v-model="customElementsForm.radio" name="sample-radio" type="radio"
-            :options="{ one: 'One', two: 'Two' }" />
-        </FormField>
-
-        <BaseDivider />
-
-        <FormField label="Switch">
-          <FormCheckRadioGroup v-model="customElementsForm.switch" name="sample-switch" type="switch"
-            :options="{ one: 'One', two: 'Two' }" />
-        </FormField>
-
-        <BaseDivider />
-
-        <FormFilePicker v-model="customElementsForm.file" label="Upload" />
       </CardBox>
 
-      <SectionTitle>Form with status example</SectionTitle>
-
-      <CardBox class="md:w-7/12 lg:w-5/12 xl:w-4/12 shadow-2xl md:mx-auto" is-form is-hoverable
-        @submit.prevent="formStatusSubmit">
-        <NotificationBarInCard :color="formStatusOptions[formStatusCurrent]"
-          :is-placed-with-header="formStatusWithHeader">
-          <span><b class="capitalize">{{ formStatusOptions[formStatusCurrent] }}</b> state</span>
-        </NotificationBarInCard>
-        <FormField label="Fields">
-          <FormControl v-model="form.name" :icon-left="mdiAccount" help="Your full name" placeholder="Name" />
-        </FormField>
-
-        <template #footer>
-          <BaseButton label="Trigger" type="submit" color="info" />
-        </template>
-      </CardBox>
+      <BaseButtons class="mt-5">
+        <BaseButton type="submit" color="info" label="Submit" />
+        <BaseButton type="reset" color="info" outline label="Reset" />
+      </BaseButtons>
     </SectionMain>
   </LayoutAuthenticated>
 </template>
