0533574c06df987c0a822a0d11f4015d62ddce74
Ruizhen Gu <ravenna1031@gmail.com>
2024-01-10 14:08:20 +0000

    frontend add manual check form

--- gradeer-plus-frontend/src/views/FormsView.vue
+++ gradeer-plus-frontend/src/views/FormsView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { reactive, ref } from 'vue'
-import { mdiBallotOutline, mdiAccount, mdiMail, mdiGithub } from '@mdi/js'
+import { mdiBallotOutline, mdiAccount, mdiMail, mdiUpload } from '@mdi/js'
 import SectionMain from '@/components/SectionMain.vue'
 import CardBox from '@/components/CardBox.vue'
+import CardBoxComponentTitle from '@/components/CardBoxComponentTitle.vue'
 import FormCheckRadioGroup from '@/components/FormCheckRadioGroup.vue'
 import FormFilePicker from '@/components/FormFilePicker.vue'
 import FormField from '@/components/FormField.vue'
@@ -15,19 +16,25 @@ import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
 import NotificationBarInCard from '@/components/NotificationBarInCard.vue'
 
-const selectOptions = [
-  { id: 1, label: 'Business development' },
-  { id: 2, label: 'Marketing' },
-  { id: 3, label: 'Sales' }
-]
+// const typeOptions = [
+//   { id: 1, label: 'ManualCheck' },
+// ]
 
 const form = reactive({
-  name: 'John Doe',
-  email: 'john.doe@example.com',
-  phone: '',
-  department: selectOptions[0],
-  subject: '',
-  question: ''
+  type: 'ManualCheck',
+  name: 'Reporting quality (to console) of answers to the questions',
+  prompt: 'Reporting quality (to console) of answers to the questions:',
+  weight: 1,
+  maxRange: 10,
+  feedbackValues: [
+    { 'score': 1.0, 'feedback': 'Outstanding reporting of answers to questions to console with excellent formatting.' },
+    { 'score': 0.8, 'feedback': 'Good reporting of answers to the questions to console with some formatting issues.' },
+    { 'score': 0.5, 'feedback': 'Adequate reporting of answers to the questions to console but poor readability .' },
+    { 'score': 0.0, 'feedback': 'Poor (or no) reporting of answers to the questions to console.' }
+  ],
+  arbitraryFeedback: 'False',
+  priority: 38,
+  checkGroup: 'FD Reporting'
 })
 
 const customElementsForm = reactive({
@@ -52,41 +59,66 @@ const formStatusSubmit = () => {
     ? formStatusCurrent.value + 1
     : 0
 }
+
+const uploadFile = () => {
+  console.log("upload file")
+}
 </script>
 
 <template>
   <LayoutAuthenticated>
     <SectionMain>
       <SectionTitleLineWithButton :icon="mdiBallotOutline" title="Forms example" main>
-        <BaseButton
-          href="https://github.com/justboil/admin-one-vue-tailwind"
-          target="_blank"
-          :icon="mdiGithub"
-          label="Star on GitHub"
-          color="contrast"
-          rounded-full
-          small
-        />
+        <BaseButton target="_blank" :icon="mdiUpload" label="Upload Checks" color="contrast" rounded-full small
+          @click="uploadFile" />
       </SectionTitleLineWithButton>
       <CardBox form @submit.prevent="submit">
-        <FormField label="Grouped with icons">
-          <FormControl v-model="form.name" :icon="mdiAccount" />
-          <FormControl v-model="form.email" type="email" :icon="mdiMail" />
+        <CardBoxComponentTitle title="Check 1" />
+
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Type</label>
+          <input v-model="form.type" class="p-2 border rounded flex-grow-0 w-1/3" />
+        </div>
+
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Name</label>
+          <input v-model="form.name" class="p-2 border rounded flex-grow-0 w-5/6" />
+        </div>
+
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Prompt</label>
+          <input v-model="form.prompt" class="p-2 border rounded flex-grow-0 w-5/6" />
+        </div>
+
+        <FormField label="Feedback Values">
+          <div v-for="(item, index) in form.feedbackValues" :key="index" class="mb-4 p-4 border border-black rounded-lg">
+            <div class="flex flex-row items-center mb-2">
+              <label for="score-{{ index }}" class="font-semibold flex-grow-0 w-1/6">Score</label>
+              <input id="score-{{ index }}" v-model="item.score" type="number" step="0.1" min="0" max="1"
+                class="p-2 border rounded flex-grow-0 w-1/3" />
+            </div>
+            <div class="flex flex-row items-center mb-2">
+              <label for="feedback-{{ index }}" class="font-semibold flex-grow-0 w-1/6">Feedback</label>
+              <input id="feedback-{{ index }}" v-model="item.feedback" rows="1"
+                class="p-2 border rounded flex-grow-0 w-5/6" />
+            </div>
+          </div>
         </FormField>
 
-        <FormField label="With help line" help="Do not enter the leading zero">
-          <FormControl v-model="form.phone" type="tel" placeholder="Your phone number" />
-        </FormField>
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Arbitrary Feedback</label>
+          <input v-model="form.arbitraryFeedback" class="p-2 border rounded flex-grow-0 w-1/3" />
+        </div>
 
-        <FormField label="Dropdown">
-          <FormControl v-model="form.department" :options="selectOptions" />
-        </FormField>
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Priority</label>
+          <input v-model="form.priority" class="p-2 border rounded flex-grow-0 w-1/3" />
+        </div>
 
-        <BaseDivider />
-
-        <FormField label="Question" help="Your question. Max 255 characters">
-          <FormControl type="textarea" placeholder="Explain how we can help you" />
-        </FormField>
+        <div class="flex flex-row items-center mb-2">
+          <label class="font-bold flex-grow-0 w-1/6">Check Group</label>
+          <input v-model="form.checkGroup" class="p-2 border rounded flex-grow-0 w-1/3" />
+        </div>
 
         <template #footer>
           <BaseButtons>
@@ -102,33 +134,22 @@ const formStatusSubmit = () => {
     <SectionMain>
       <CardBox>
         <FormField label="Checkbox">
-          <FormCheckRadioGroup
-            v-model="customElementsForm.checkbox"
-            name="sample-checkbox"
-            :options="{ lorem: 'Lorem', ipsum: 'Ipsum', dolore: 'Dolore' }"
-          />
+          <FormCheckRadioGroup v-model="customElementsForm.checkbox" name="sample-checkbox"
+            :options="{ lorem: 'Lorem', ipsum: 'Ipsum', dolore: 'Dolore' }" />
         </FormField>
 
         <BaseDivider />
 
         <FormField label="Radio">
-          <FormCheckRadioGroup
-            v-model="customElementsForm.radio"
-            name="sample-radio"
-            type="radio"
-            :options="{ one: 'One', two: 'Two' }"
-          />
+          <FormCheckRadioGroup v-model="customElementsForm.radio" name="sample-radio" type="radio"
+            :options="{ one: 'One', two: 'Two' }" />
         </FormField>
 
         <BaseDivider />
 
         <FormField label="Switch">
-          <FormCheckRadioGroup
-            v-model="customElementsForm.switch"
-            name="sample-switch"
-            type="switch"
-            :options="{ one: 'One', two: 'Two' }"
-          />
+          <FormCheckRadioGroup v-model="customElementsForm.switch" name="sample-switch" type="switch"
+            :options="{ one: 'One', two: 'Two' }" />
         </FormField>
 
         <BaseDivider />
@@ -138,27 +159,14 @@ const formStatusSubmit = () => {
 
       <SectionTitle>Form with status example</SectionTitle>
 
-      <CardBox
-        class="md:w-7/12 lg:w-5/12 xl:w-4/12 shadow-2xl md:mx-auto"
-        is-form
-        is-hoverable
-        @submit.prevent="formStatusSubmit"
-      >
-        <NotificationBarInCard
-          :color="formStatusOptions[formStatusCurrent]"
-          :is-placed-with-header="formStatusWithHeader"
-        >
-          <span
-            ><b class="capitalize">{{ formStatusOptions[formStatusCurrent] }}</b> state</span
-          >
+      <CardBox class="md:w-7/12 lg:w-5/12 xl:w-4/12 shadow-2xl md:mx-auto" is-form is-hoverable
+        @submit.prevent="formStatusSubmit">
+        <NotificationBarInCard :color="formStatusOptions[formStatusCurrent]"
+          :is-placed-with-header="formStatusWithHeader">
+          <span><b class="capitalize">{{ formStatusOptions[formStatusCurrent] }}</b> state</span>
         </NotificationBarInCard>
         <FormField label="Fields">
-          <FormControl
-            v-model="form.name"
-            :icon-left="mdiAccount"
-            help="Your full name"
-            placeholder="Name"
-          />
+          <FormControl v-model="form.name" :icon-left="mdiAccount" help="Your full name" placeholder="Name" />
         </FormField>
 
         <template #footer>
