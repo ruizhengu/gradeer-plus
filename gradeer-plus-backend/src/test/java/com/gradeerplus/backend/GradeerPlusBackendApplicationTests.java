b732d37b457538a2f91ac7f63cffa63f48d5eb3a
Ruizhen Gu <ravenna1031@gmail.com>
2024-01-22 15:04:23 +0000

    backend init

---
+++ gradeer-plus-backend/src/test/java/com/gradeerplus/backend/GradeerPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradeerplus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeerPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
